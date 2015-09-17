175e082bf3f9f149a078bf225fd21c04a9d8a48b
Irene <ichen@athenahealth.com>
2015-09-17 12:03:13 -0700

    Re-added Bridging header and updated tests to be struct

--- ImaginaryAnimalsListerTests/TableViewControllerTests.swift
+++ ImaginaryAnimalsListerTests/TableViewControllerTests.swift
@@ -9,6 +9,10 @@
 import XCTest
 @testable import ImaginaryAnimalsLister
 
+struct TestMyAnimalsLoader: AnimalsLoader {
+    var url:NSURL? = NSBundle.mainBundle().URLForResource("Animals", withExtension: "json")
+}
+
 class TableViewControllerTests: XCTestCase {
     
     var tableViewController:TableViewController?
@@ -17,7 +21,7 @@ class TableViewControllerTests: XCTestCase {
         super.setUp()
         
         tableViewController = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle()).instantiateViewControllerWithIdentifier("TableViewController") as? TableViewController
-        tableViewController?.animalsArray = AnimalsLoader().loadAnimals()
+        tableViewController?.animalsArray = TestMyAnimalsLoader().loadAnimals()
     }
     
     override func tearDown() {
@@ -29,13 +33,6 @@ class TableViewControllerTests: XCTestCase {
         
         XCTAssert(tableViewController?.animalsArray.count > 0)
         XCTAssertEqual(tableViewController?.animalsArray.count, tableViewController?.tableView .numberOfRowsInSection(0))
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
     }
     
 }
